41d1e7c08f1be16c4d8f992d714d104b53c68a15
Smith <Devin.Smith@quartech.com>
2020-01-22 11:56:58 -0800

    Cleanup unused code. Migrate additional js files to tsx. update deploy params to point to correct pims url Cleanup guest page css Fix logout button.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Keycloak from 'keycloak-js';
+import Keycloak, { KeycloakInstance } from 'keycloak-js';
 import { KeycloakProvider } from 'react-keycloak';
 import { Spinner } from 'react-bootstrap';
-import { KEYCLOAK } from 'constants/environment'
 
-const keycloak = new Keycloak('keycloak.json');
+//@ts-ignore
+const keycloak:KeycloakInstance = new Keycloak('keycloak.json');
 const Index = () => {
-    console.log(process.env);
     return <KeycloakProvider
         keycloak={keycloak}
         LoadingComponent={<Spinner animation="border"></Spinner>}>
