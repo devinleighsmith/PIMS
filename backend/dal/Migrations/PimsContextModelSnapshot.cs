28062e73db84d603168177fae8cc022ddc144d6f
Devin Smith <devinleighsmith@gmail.com>
2021-02-09 10:12:04 -0800

    Create foreign key mapping to property types table.

--- backend/dal/Migrations/PimsContextModelSnapshot.cs
+++ backend/dal/Migrations/PimsContextModelSnapshot.cs
@@ -472,9 +472,6 @@ namespace Pims.Dal.Migrations
                     b.Property<DateTime?>("UpdatedOn")
                         .HasColumnType("DATETIME2");
 
-                    b.Property<int>("propertyTypeId")
-                        .HasColumnType("int");
-
                     b.HasKey("Id");
 
                     b.HasIndex("AddressId");
@@ -1117,9 +1114,6 @@ namespace Pims.Dal.Migrations
                     b.Property<string>("ZoningPotential")
                         .HasColumnType("nvarchar(50)")
                         .HasMaxLength(50);
-
-                    b.Property<int>("propertyTypeId")
-                        .HasColumnType("int");
 
                     b.HasKey("Id");
 
